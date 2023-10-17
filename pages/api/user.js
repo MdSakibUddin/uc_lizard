596cd9a366750a91faa38834a3b5eccabba50ac6
Md Sakib Uddin <md.sakib.uddin@g.bracu.ac.bd>
2023-10-18 04:14:00 +1100

    connecting error still going

--- pages/api/user.js
+++ pages/api/user.js
@@ -11,8 +11,39 @@ const connection = mysql.createPool({
 
 export default async (req, res) => {
   try {
-    const [rows, fields] = await connection.query("SELECT * FROM H_cages");
-    res.json(rows);
+    const searchQuery = `
+        (SELECT AltSpecimenID AS result FROM A_specimens WHERE AltSpecimenID LIKE ? OR Status LIKE ? OR Priority LIKE ? OR Category LIKE ? OR Family LIKE ? OR ... )
+        UNION
+        (SELECT SpecimenID AS result FROM B_samples WHERE SpecimenID LIKE ? OR SampleID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT ClutchID AS result FROM C_clutches WHERE ClutchID LIKE ? OR AltClutchID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT Record_ID AS result FROM D_metrics WHERE SpecimenID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT RefID AS result FROM E_sextests WHERE SpecimenID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT SpecimenID AS result FROM F_cultures WHERE SpecimenID LIKE ? OR CultureID LIKE ? OR Genus LIKE ? OR ...)
+        UNION
+        (SELECT Key AS result FROM G_genomics WHERE SampleID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT RecNo AS result FROM H_cages WHERE SpecimenID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT RefID AS result FROM I_treatments WHERE SpecimenID LIKE ? OR Genus LIKE ? OR Species LIKE ? OR ...)
+        UNION
+        (SELECT Ref AS result FROM Z_cagelist WHERE Room LIKE ? OR Cage LIKE ?)
+        UNION
+        (SELECT UC_ID AS result FROM Z_freezerlist WHERE UC_ID LIKE ? OR Freezer LIKE ?)
+    `;
+
+    const queryParams = Array(120).fill(`%${searchValue}%`); // This should be the total number of columns you're searching across all tables
+
+    db.query(searchQuery, queryParams, (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Database error." });
+      }
+      res.json(results);
+    });
   } catch (error) {
     res.status(500).json({ message: "Error connecting to db", error });
   }
